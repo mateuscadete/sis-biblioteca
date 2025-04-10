8f11eb3a7b02b17f5a63749c8a7c2a834cdd1aab
JoelQuia <oliver.joel2020@gmail.com>
2025-04-09 22:34:50 -0300

    estilizando contato

--- resources/views/layout/contato.blade.php
+++ resources/views/layout/contato.blade.php
@@ -4,35 +4,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Biblioteca Etec Zona Leste</title>
+    <title>Contato</title>
     <link rel="stylesheet" href="{{ asset('css/navbar1.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/contato.css') }}">
 </head>
 
 <body>
+    @include('includes.navbar1')
 
-    <header>
-        @include('includes.navbar1')
-    </header>
-    <form action="{{ route('contato.submit') }}" method="POST">
+    <div class="container">
+
+    <h2>Entre em Contato com a nossa biblioteca</h2>
+
+    <form class="contato" action="{{ route('contato.submit') }}" method="POST">
         @csrf
-        <div>
+       
+        
             <label for="nome">Nome:</label>
-            <input type="text" id="nome" name="nome" required>
-        </div>
-        <div>
+            <input type="text" class="name" id="nome" name="nome" required>
+        
             <label for="email">E-mail:</label>
-            <input type="email" id="email" name="email" required>
-        </div>
-        <div>
+            <input type="email" class= "email" id="email" name="email" required>
+        
             <label for="assunto">Assunto:</label>
-            <textarea id="assunto" name="assunto" rows="5" required></textarea>
-        </div>
-        <div>
-            <button type="submit">Enviar</button>
-        </div>
+            <textarea id="assunto" class="assunto" name="assunto" rows="5" required></textarea>
+        
+            <button class="enviar" type="submit">Enviar</button>
+        
     </form>
-    </header>
+
+    </div>
+    
 
     @include('includes.footer')
 
