5225f2036d6a4a5b3b8485489d1a887e12054ac5
Mateus Cadete <mateus_cadete@outlook.com>
2025-04-16 20:44:34 -0300

    Commitando a base para a lógica do empréstimo de livros

--- database/migrations/2025_04_16_230451_create_books_table.php
+++ database/migrations/2025_04_16_230451_create_books_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('books', function (Blueprint $table) {
+        Schema::create('livros', function (Blueprint $table) {
             $table->id();
             $table->string('titulo');
             $table->integer('qtde'); // Quantidade disponível
