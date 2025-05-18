074c0706ce234c013659b582cc27bac1f8ef8e8a
Mateus Cadete <mateus_cadete@outlook.com>
2025-05-17 21:01:12 -0300

    Commitando atualização na lógica de empréstimo de livros

--- app/Models/User.php
+++ app/Models/User.php
@@ -63,6 +63,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
+            'is_admin' => 'boolean',
             'password' => 'hashed',
         ];
     }
