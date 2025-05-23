f17e308e9359b780b65610abdc613ac097c2f06b
nxgueira <vnpimenta@gmail.com>
2025-05-22 21:54:16 -0300

    Finalizando o sobre

--- resources/views/layout/sobre.blade.php
+++ resources/views/layout/sobre.blade.php
@@ -4,12 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sobre Nós</title>
-    
+        <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+
     
 </head>
 <body>
    @include('includes.navbar1')
-   
+   <header>
+        <img src="{{url('imagens/biblioteca.jpg')}}" style="object-fit: cover; width: 100%; height: 100%; ">
+
+        <ul class="forms">
+            <li><a href="{{ route('user.ajuda') }}">Ajuda</a></li>
+            <li><a href="{{ route('user.login') }}">Login</a></li>
+            <li><a href="{{ route('user.cadastro') }}">Cadastrar</a></li>
+        </ul>
+
+        @include('includes.navbar')
+
+        <div class="titulo">
+            <h1>Conheça um pouco<br> sobre a iniciativa<br>da plataforma</h1>
+
+        </div>
+
+
+
+    </header>
+    <main>
+           <h1> Uma biblioteca <br> com um melhor <br> serviço </h1>
+
     
 </body>
 </html>
