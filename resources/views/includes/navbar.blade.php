c7dce8da16e85b7ac7efe926305a7da2d3761ad0
JoelQuia <oliver.joel2020@gmail.com>
2025-06-16 20:32:56 -0300

    colocando link novo

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -20,8 +20,10 @@
         <ul class="links" id="navbar-links">
 
             <li><a href="{{ route('layout.acervo') }}">Nosso Acervo</a></li>
+            <li><a href="{{route('emprestimos.index')}}">Meus Empréstimos</a></li>
             <li><a href="{{ route('layout.sobre') }}">Sobre</a></li>
             <li><a href="{{ route('contato.submit') }}">Contato</a></li>
+
             @auth
             @if (Auth::user() ->is_admin)
             <li><a href="{{route('dashboard')}}">Cadastrar Livros</a> </li>
@@ -51,11 +53,6 @@
                     </svg>
                 </a>
 
-                <a href="{{ route('emprestimos.index') }}" class="icon-button" aria-label="Acessar Empréstimo">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
-                        <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783" />
-                    </svg>
-                </a>
 
                 <div class="user-conta">
                     <button class="icon-button" aria-label="Acessar Minha Conta" aria-haspopup="true" aria-expanded="false" id="user-menu-button">
@@ -83,6 +80,23 @@
                         @endauth
                     </div>
                 </div>
+
+                <div class="user-conta">
+                    <button class="icon-button" aria-label="Acessar Área do Admin" aria-haspopup="true" aria-expanded="false" id="user-menu-button">
+                
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-book-half" viewBox="0 0 16 16">
+                      <path d="M8.5 2.687c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783"/>
+                  </svg>
+                
+                </button>
+                <div class="logado" id="user-dropdown-content">
+
+                        <a href="{{ route('dashboard') }}" class="logar">Cadastrar Livro</a>
+                        <a href="{{ route('layout.show') }}" class="cadastrar">livros Cadastrados</a>
+                        
+                        
+                </div>
+                    </div>
             </div>
         </div>
     </nav>
